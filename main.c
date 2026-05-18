7be28d6f395c27565c9de3cba980d1b599c0f8b1
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2026-05-18 14:24:53 -0300

    explicando as funções
    Updated function declarations and added comments for clarity.

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
-#include <stdio.h>   // padrão entrada(input) e saída(output)
-#include <stdlib.h>  // biblioteca padrão  - manipulação de arquivos, malloc/realloc;
-#include <string.h>  //
-#include <stdbool.h> //
+#include <stdio.h>
+#include <string.h>
 
 // CONSTANTES
 #define MAX_NOME_ARQUIVO 15
@@ -9,11 +7,41 @@
 #define MAX_COLUNAS 40
 
 // FUNÇOES BACANAS QUE AINDA NÃO FAZEM NADA
-void lerArquivo();
+void lerArquivo(); 
+/* {
+    função pra reconhecer o arquivo e mostrar ao usuário;
+    busca o nome do arquivo; 
+    mostra no terminal o nome do arquivo e o conteúdo do arquivo;
+    respeitar o MAX_LINHAS e MAX_COLUNAS 
+}*/
+
 void criarArquivo();
+/* {
+    função que cria um novo arquivo na memória;
+    pergunta ao usuário o nome do arquivo;
+    pergunta ao usuário o que ele gostaria de colocar como conteúdo;
+    enter para uma nova linha;
+    condição de parada: enter duplo (?);
+    respeitar o MAX_LINHAS e MAX_COLUNAS 
+}*/
+
 void editarArquivo();
-void importarArquivo();
+/* {
+    pergunta ao usuário o nome do arquivo;
+    busca o nome do arquivo na memória. se o arquivo for inexistente, avisar ao usuário;
+    abre o arquivo e permite que o usuário edite o nome e o conteúdo existente, podendo apagar tudo ou só adicionar;
+    salva as alterações e deleta o conteúdo anterior
+}*/
+
+void importarArquivo(); // retirar talvez? não entendo qual o propósito dessa opção e vai ser dificil afuzeu fazer
+
 void apagarArquivo();
+/* {
+    pede ao usuário o nome do arquivo;
+    busca o arquivo, se for inexistente, avisar ao usuário;
+    pergunta ao usuário se ele tem certeza que quer excluir o arquivo;
+    se sim, apaga o conteúdo total do arquivo (nome e conteúdo interno)
+}*/
 
 // MENU BEM RESUMIDO
 int main()

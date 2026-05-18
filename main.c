497f4a8a51c662970baab7e70a66379a6d6cf369
Luana Gonçalves <144810083+luanasgoncalves@users.noreply.github.com>
2026-05-18 14:05:52 -0300

    menu estrutura
    Refactored main function and added menu options.

--- main.c
+++ main.c
@@ -3,44 +3,66 @@
 #include <string.h>  //
 #include <stdbool.h> //
 
-/*
+// CONSTANTES
+#define MAX_NOME_ARQUIVO 15
+#define MAX_LINHAS 30
+#define MAX_COLUNAS 40
 
-Funções principais
-1. Abrir um arquivo para leitura;
-2. Criar um arquivo para escrita;
-3. Editar um arquivo;
-4. Excluir um arquivo;
-5. Importar um arquivo;
-6. Encerrar um programa;
+// FUNÇOES BACANAS QUE AINDA NÃO FAZEM NADA
+void lerArquivo();
+void criarArquivo();
+void editarArquivo();
+void importarArquivo();
+void apagarArquivo();
 
-//alocação estática
-int vetor[5][6];
-int vetor[][] = { {1, 2, 3,7,8,9}, {4, 5, 6}, {}, {}, {} };
-//alocação dinâmica
-int **vetor = {{}, {}, {}, {}};
-
-printf("* ---------------- MENU DE OPÇÕES ---------------- *\n");
--printf("1. Abrir um arquivo para leitura;\n");
--printf("2. Criar um arquivo para escrita;\n");
--printf("3. Editar um arquivo;\n");
--printf("4. Excluir um arquivo;\n");
--printf("5. Importar um arquivo;\n");
--printf("6. Encerra bool verdadeiro = false; um programa;\n");
-
-*/
-
+// MENU BEM RESUMIDO
 int main()
 {
+    char nomeArquivo[MAX_NOME_ARQUIVO];
+    int menu = 0;
 
-    char *opcoesMenu[] = {
-        "1. Abrir um arquivo para leitura", "2. Criar um arquivo para escrita", "3. Editar um arquivo",
-        "4. Excluir um arquivo", "5. Importar um arquivo", "6. Encerrar um programa"
-    };
-    printf("* ---------------- Bem-vindo ao Editor de Texto CLI! ---------------- *\n");
-    printf("* ---------------- Seleciona aí, miserável ---------------- *\n");
-    for (int i = 0; i < 6; i++) {
-        printf("%s\n", opcoesMenu[i]);
+    while (menu != 6)
+    {
+        printf("1. Ler arquivo;\n");
+        // scanf("%[^\n]s", nomeArquivo);
+        printf("2. Criar arquivo;\n");
+        printf("3. Editar arquivo;\n");
+        printf("4. Importar arquivo;\n");
+        printf("5. Apagar arquivo;\n");
+        printf("6. Sair do programa;\n");
+
+        scanf("%d", &menu);
+
+        switch(menu) 
+        {
+            case 1:
+            lerArquivo(); // criar funçao
+            break;
+
+            case 2:
+            criarArquivo();
+            break;
+
+            case 3:
+            editarArquivo();
+            break;
+
+            case 4:
+            importarArquivo();
+            break;
+
+            case 5:
+            apagarArquivo();
+            break;
+
+            case 6:
+            printf("Encerrando programa...");
+            break;
+
+            default:
+            printf("Valor inválido.");
+        }
     }
-    printf("\n > ");
 
+    return 0;
 }
